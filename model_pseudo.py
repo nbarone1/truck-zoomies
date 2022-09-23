177d7adf0fd8c510c5e16889827200ec17e6d793
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2022-09-23 13:06:43 -0500

    notes

--- model_pseudo.py
+++ model_pseudo.py
@@ -3,8 +3,8 @@
 # idea for model - time series with LSTM for certain parameters
 # LSTM for price, labor, fuel
 
-# Params right now: historical cost, historical fuel, temp, precipitation, market, seasonality (holiday, harvest season), DAT frequency, maitenance proxy 
-# Cost, Market, (DAT) - ALX
+# Params right now: historical cost, historical fuel, temp, precipitation, market, seasonality (holiday, harvest season), DAT frequency, maitenance proxy, load type 
+# Cost, Market, (DAT), load type - ALX/ALC data
 # DAT - DAT database
 # temp, precipitation - OpenWeatherAPI???
 # seasonality - build calendar
