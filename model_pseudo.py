f4c032e1af68d9261542d4eebd291f37c8dee18d
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2022-09-23 13:18:40 -0500

    notes

--- model_pseudo.py
+++ model_pseudo.py
@@ -6,6 +6,6 @@
 # Params right now: historical cost, historical fuel, temp, precipitation, market, seasonality (holiday, harvest season), DAT frequency, maitenance proxy, load type 
 # Cost, Market, (DAT), load type - ALX/ALC data
 # DAT - DAT database
-# temp, precipitation - OpenWeatherAPI???
+# temp, precipitation - visual crossing weather
 # seasonality - build calendar
 # maitenance statistics - ???
