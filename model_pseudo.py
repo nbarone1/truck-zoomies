436d12d006ce06582bb70ccfd89fa39b8ee70916
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2022-09-23 14:00:43 -0500

    notes

--- model_pseudo.py
+++ model_pseudo.py
@@ -1,5 +1,10 @@
 # MODEL OUTLINE/PSUEDO CODE
 
+# import statements, assuming we go with tensorflow
+import requests
+import tensorflow as tf
+
+
 # idea for model - time series with LSTM for certain parameters
 # LSTM for price, labor, fuel
 
