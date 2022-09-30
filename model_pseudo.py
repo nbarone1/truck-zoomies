33abe9924fec7f8ed22e56af8c8cd4ad10fbc0f7
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2022-09-30 15:47:15 -0500

    add keras

--- model_pseudo.py
+++ model_pseudo.py
@@ -8,6 +8,8 @@ import datetime
 import requests
 import http.client
 import tensorflow as tf
+from tensorflow import keras
+from keras import layers
 import time as t
 import numpy as np
 import matplotlib.pyplot as plt
