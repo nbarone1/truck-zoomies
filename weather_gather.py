311a085ed775a1d69f082dc713861604b9c52c7c
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2022-10-14 09:17:27 -0500

    weather

--- weather_gather.py
+++ weather_gather.py
@@ -3,4 +3,16 @@
 
 import pandas as pd
 import numpy as np
+import sys
+
+# Idea is to use Visual Crossing
+# decision on weather API determines following imports, API Keys, etc
+
+import urllib.error
+import urllib.request
+
+BaseUrl = ''
+
+API_KEY = ''
+
 
