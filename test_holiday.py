4a8f8c6ff46f4cdf48c0e9de2b6842c0ef5cb9a4
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2022-10-14 09:06:30 -0500

    weather time

--- test_holiday.py
+++ test_holiday.py
@@ -3,7 +3,7 @@
 import pandas as pd
 from pandas.tseries.holiday import USFederalHolidayCalendar as calendar
 
-dr = pd.date_range(start='2015-07-01', end='2015-07-31')
+dr = pd.date_range(start='2023-07-01', end='2024-12-27')
 df = pd.DataFrame()
 df['Date'] = dr
 
---
+++ weather_gather.py
@@ -0,0 +1,6 @@
+# API pulling
+# Done seperately from data preperation for Readability
+
+import pandas as pd
+import numpy as np
+
