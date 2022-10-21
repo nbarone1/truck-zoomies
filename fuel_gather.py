f6f0dde3ebdee3fbaec1857f7c8d3e873f49f4b3
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2022-10-21 13:18:41 -0500

    fuel

---
+++ fuel_gather.py
@@ -0,0 +1,14 @@
+# Fuel API
+# Father Fuel API for fuel prices in origination and destination state
+
+# Finding API that works efficiently
+
+# Imports
+import pandas as pd
+import numpy as np
+
+
+# API set ip
+BaseUrlF = ''
+
+API_KEYF = ''
