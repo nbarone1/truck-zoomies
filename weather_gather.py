a4f2c86ce9484d86e49972c3477e9a4078fb07f2
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2022-10-28 10:39:17 -0500

    meep

--- weather_gather.py
+++ weather_gather.py
@@ -4,15 +4,34 @@
 import pandas as pd
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
+from meteostat import Stations, Daily
+from datetime import datetime
+
 
 # Idea is to use Visual Crossing
 # decision on weather API determines following imports, API Keys, etc
 
-import urllib.error
-import urllib.request
+# import urllib.error
+# import urllib.request
 
-BaseUrlW = ''
+# BaseUrlW = ''
 
-API_KEYW = ''
+# API_KEYW = ''
 
+start = datetime(2022,1,1)
+end = datetime(2022,10,27)
 
+data = Daily('10637',start,end)
+data =data.fetch()
+
+data['tavg']=(data['tavg']*9/5)+32
+data['prcp']=data['prcp']/25.4
+
+fig,ax1 = plt.subplots()
+
+ax2 = ax1.twinx()
+ax1.plot(data['tavg'])
+ax2.plot(data['prcp'],'g-')
+
+plt.show()
