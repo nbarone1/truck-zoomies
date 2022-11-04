df4737a8ffed8d71a193c900fc734452f9d40c81
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2022-11-04 12:00:51 -0500

    fuel start

--- fuel_gather.py
+++ fuel_gather.py
@@ -1,14 +1,14 @@
-# Fuel API
-# Father Fuel API for fuel prices in origination and destination state
+# Gather Fuel Data to populate data
+# Get Average Fuel Price for that week based around 
 
-# Finding API that works efficiently
-
-# Imports
+# The above code is importing the necessary libraries for the code to run.
 import pandas as pd
+import requests
+import matplotlib.pyplot as plt
 import numpy as np
+from datetime import date
+import matplotlib.ticker as ticker
 
-
-# API set ip
-BaseUrlF = ''
-
-API_KEYF = ''
+# This is the API key that is needed to access the data from the EIA website.
+API_KEY = '0JWYgxKlukehAgfhGxGGWgeoARIQhUCyU1JPRke9'
+url = 'https://api.eia.gov/v2/petroleum/pri/gnd/data/?api_key'+API_KEY
