1daaef8de9eb2ce344a8d2c37296b716f9e1512b
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2022-11-04 12:59:11 -0500

    fuel start

--- fuel_gather.py
+++ fuel_gather.py
@@ -10,5 +10,41 @@ from datetime import date
 import matplotlib.ticker as ticker
 
 # This is the API key that is needed to access the data from the EIA website.
-API_KEY = '0JWYgxKlukehAgfhGxGGWgeoARIQhUCyU1JPRke9'
-url = 'https://api.eia.gov/v2/petroleum/pri/gnd/data/?api_key'+API_KEY
+API_KEY = '0JWYgxKIukehAgfhGxGGWgeoARIQhUCyU1JPRke9'
+url = 'https://api.eia.gov/v2/petroleum/pri/gnd/data/api_key='+API_KEY
+print(url)
+
+# A dictionary that is used to pass parameters to the API.
+header = {
+            "frequency": "weekly",
+            "data": [
+                "value"
+            ],
+            "facets": {
+                "product": [
+                    "EPD2D"
+                ]
+            },
+            "start": "2021-01-01",
+            "end": None,
+            "sort": [
+                {
+                    "column": "period",
+                    "direction": "desc"
+                },
+                {
+                    "column": "duoarea",
+                    "direction": "asc"
+                }
+            ],
+            "offset": 0,
+            "length": 5000,
+            "api-version": "2.0.3"
+        }
+
+def get_fuel():
+    f = requests.get(url,header)
+    print(f)
+    return
+
+get_fuel()
