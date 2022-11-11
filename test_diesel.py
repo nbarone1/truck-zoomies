5b0311a042f5f69398d6ff2749235ae305144bd8
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2022-11-11 15:36:39 -0600

    test diesel

--- test_diesel.py
+++ test_diesel.py
@@ -8,12 +8,12 @@ PATH = os.getcwd()
 
 diesel_data = pd.read_csv(PATH+'\diesel_test_1.csv')
 
-diesel_data['Date'] = pd.to_datetime(diesel_data['Date'])
+diesel_data['Date'] = pd.to_datetime(diesel_data['Date'],format='%m/%d/%Y')
 diesel_data.set_index('Date').resample('D').ffill().reset_index()
 
-print(diesel_data)
-
-lists = [74135,60660,60640,20002]
+zipcodes = [74135,60660,60640,20002]
+Date = pd.DataFrame(['3/7/2021','6/22/2022','10/4/2022','10/5/2022'],columns=['Date'])
+Date['Date'] = pd.to_datetime(Date['Date'],format='%m/%d/%Y')
 
 def assign_duoarea(lists):
     """
@@ -22,6 +22,7 @@ def assign_duoarea(lists):
     :param lists: a list of zipcodes
     """
     engine = SearchEngine()
+    da = []
     duoareas = {'R10X':['CT','ME','MA','NH','RI','VT'], 
             'R10Y':['DE','DC','MD','NJ','NY','PA'],
             'R10Z':['FL','GA','NC','SC','VA','WV'],
@@ -33,6 +34,25 @@ def assign_duoarea(lists):
     for i in range(0,len(lists)):
         st = engine.by_zipcode(lists[i])
         kpos = [k for k,v in duoareas.items() if st.state_abbr in v]
-        print(kpos[0])
+        da.append(kpos[0])
+    return da
         
-assign_duoarea(lists)
+DuoArea = pd.DataFrame(assign_duoarea(zipcodes),columns=['DuoArea'])
+data = pd.concat([Date,DuoArea],1)
+
+print(diesel_data['Date'])
+
+def pricepull(data,reference):
+    pricelist = pd.DataFrame()
+    datelist = list(reference['Date'])
+    for i in range(0,len(data['Date'])):
+        day = data.iloc[i][0]
+        day = day.strftime('%Y-%m-%d')
+        dayindex = datelist.index(day)
+        region = int(reference.columns.get_loc(data.iloc[i][1]))
+        print(region)
+        price = reference.iloc[dayindex][region]
+        pricelist[i] = price
+    return pricelist
+
+prices = pricepull(data,diesel_data)
