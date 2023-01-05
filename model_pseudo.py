b992bc360a0e55d0aaa8e88dcf0ce6c6ea8a239b
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2023-01-04 18:05:15 -0600

    added timing

--- model_pseudo.py
+++ model_pseudo.py
@@ -17,45 +17,45 @@ st = time.process_time()
 test_data = pd.read_csv(PATH+'\zipcode_test.csv')
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("Read function time of ",t)
 
 st = time.process_time()
 test_orig = dprep.zip_set(test_data['ORIG'])
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("Zip function time of ",t)
 
 st = time.process_time()
 test_dest = dprep.zip_set(test_data['DEST'])
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("Zip function time of ",t)
 
 st = time.process_time()
 dtest = dprep.holiday_dataframe(test_data['%Calendar Date'])
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("Holiday function time of ",t)
 
 # One-hot encoding the load type column.
 st = time.process_time()
 ltest = dprep.data_onehot(test_data['Load Type'])
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("One hot function time of ",t)
 
 # One-hot encoding the zip codes.
 st = time.process_time()
 oztest = dprep.data_onehot(test_orig)
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("One hot function time of ",t)
 
 st = time.process_time()
 dztest = dprep.data_onehot(test_dest)
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("One hot function time of ",t)
 
 # This is the weather gathering function. It takes the zip code, country, and date and returns the
 # weather data for that zip code on that date.
@@ -63,13 +63,13 @@ st = time.process_time()
 owtest = wg.wg(test_orig,"us",test_data['%Calendar Date'])
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("Weather function time of ",t)
 
 st = time.process_time()
 owtest = pd.concat([owtest['tavg'],owtest['tmin'],owtest['tmax'],owtest['prcp'],owtest['snow']],1)
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("Concat function time of ",t)
 
 print("yes")
 
@@ -77,13 +77,13 @@ st = time.process_time()
 dwtest = wg.wg(test_dest,"us",test_data['%Calendar Date'])
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("Weather function time of ",t)
 
 st = time.process_time()
 dwtest = pd.concat([dwtest['tavg'],dwtest['tmin'],dwtest['tmax'],dwtest['prcp'],dwtest['snow']],1)
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("Concat function time of ",t)
 
 print("yes")
 
@@ -92,11 +92,11 @@ st = time.process_time()
 test_result = pd.concat([ltest,owtest,dwtest,oztest,dztest],1)
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("Concat function time of ",t)
 
 # Saving the dataframe to a csv file.
 st = time.process_time()
 test_result.to_csv('zip_test_results.csv',index=False)
 et = time.process_time()
 t = et-st
-print("Function time of ",t)
+print("To CSV function time of ",t)
