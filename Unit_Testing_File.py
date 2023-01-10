b6aa7ea34670ec9b54dfb292596300ebca95ea96
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2023-01-10 13:15:15 -0600

    started testing

--- Unit_Testing_File.py
+++ Unit_Testing_File.py
@@ -13,6 +13,22 @@ import time
 import tkinter as tk
 from tkinter import filedialog
 
+# This is a GUI to select a file.
+root = tk.Tk()
+
+file_path = filedialog.askopenfilename()
+
 # Unit Test import
 import unittest as ut
+
+# importing methods from support files
+import data_prep as dprep
+import weather_gather as wg
+
+# This is a timing function to see how long it takes to read in the data.
+st = time.process_time()
+test_data = pd.read_csv(file_path)
+et = time.process_time()
+t = et-st
+print("Read function time of ",t)
 
