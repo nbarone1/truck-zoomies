dbf988fdd82c9d5e0400cf9a1221d603bc777475
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2023-01-10 12:55:53 -0600

    started testing

---
+++ Unit_Testing_File.py
@@ -0,0 +1,18 @@
+# Unit Testing File
+# Handling Unit Tests for each method to be used
+# Integration Testing will be a seperate file
+# Will be using single entry only for this step
+
+# import statements
+# assume tensorflow and keras moving forward
+import pandas as pd
+import os
+import time
+
+# Select Data File
+import tkinter as tk
+from tkinter import filedialog
+
+# Unit Test import
+import unittest as ut
+
